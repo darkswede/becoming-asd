be530e62d2429f242d705a5954952242d29d9844
Marcin Pietrucha <mpietrucha@g2a.com>
2021-02-18 18:02:34 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace becoming_asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
